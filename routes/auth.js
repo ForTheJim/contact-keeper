e120b7c4958f5c6e7a4a490aa53564f20852e1b8
Jim Olguin <jim@forthejim.com>
2020-07-26 22:06:38 -0700

    - set bcrypt and jwt for User - connect to backend in server.js - set up db.js file with mongoose info that is need to connect to backend - add jwt secret and mongoURI to default.json in config folder - Set up User model for backend db to use in User.js - set up auth for user - set up registration for user in users.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const config = require("config");
 
 // @route   GET api/auth
 // @desc    Get logged in user
@@ -11,9 +16,55 @@ router.get("/", (req, res) => {
 // @route   POST api/auth
 // @desc   Auth user and get token
 // @access  Public
-router.post("/", (req, res) => {
-  res.send("Logs user in");
-});
+router.post(
+  "/",
+  [
+    body("email", "Please include a valid email").isEmail(),
+    body(
+      "password",
+      "Password is required. Please enter your password"
+    ).exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid Credentials" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid Credentials" });
+      }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("SERVER ERROR");
+    }
+  }
+);
 
 // Export Router
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,69 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const config = require("config");
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post("/", (req, res) => {
-  res.send("Registers a User");
-});
+router.post(
+  "/",
+  [
+    body("name", "Name is required").not().isEmpty(),
+    body("email", "Please include a valid email").isEmail(),
+    body(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } else {
+      const { name, email, password } = req.body;
+      try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+          return res.status(400).json({ msg: "User already exists!" });
+        }
+        user = new User({
+          name,
+          email,
+          password,
+        });
+        // Encrypt password
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+        const payload = {
+          user: {
+            id: user.id,
+          },
+        };
+
+        jwt.sign(
+          payload,
+          config.get("jwtSecret"),
+          { expiresIn: 3600 },
+          (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+          }
+        );
+      } catch (error) {
+        console.error(error.message);
+        res.status(500).send(" SERVER ERROR ");
+      }
+    }
+  }
+);
 
 // Export Router
 module.exports = router;
